a0a40d97b16bd818e12795efb40636e6935c33b9
ajith <ajithraveendranr@gmail.com>
2022-08-03 19:07:30 +0530

    spell

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import { Index } from './Pages/Index';
-import { Home } from './Pages/Home';
-import { NotFound } from './Pages/NotFound';
-import { Route } from 'react-router-dom';
-import { Routes } from 'react-router-dom';
+import React from "react";
+import { Index } from "./Pages/Index";
+import { Home } from "./Pages/Home";
+import { NotFound } from "./Pages/NotFound";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 
- const AppRoutes = () => {
-  
-    return (
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
 
-  <Routes>
-
- <Route path='/' element={<Index/>}/>
- <Route path='/home' element={ <div id='home-page'>Home Page</div>}/>
- <Route path='*'element={<NotFound/>}/> 
-
-
-
-
-  </Routes>
-
-       )
-}
-
-export {AppRoutes};
+export { AppRoutes };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export const NavBar = () =>{
-    return(
-<nav>
-    <ul>
-    <li className='index-link'>
-        <Link to="/">index</Link>
+export const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li className="index-link">
+          <Link to="/">index</Link>
         </li>
-        <li className='home-link'>
-        <Link to='/home'>home</Link>
-       </li>
-    </ul>
-</nav>
-    )
-}
+        <li className="home-link">
+          <Link to="/home">home</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
